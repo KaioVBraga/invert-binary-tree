7f13bde294b24843b7b9da2041bc5339b36fd407
kaiovbraga <kaiovbraga2001@gmail.com>
2021-04-02 22:52:10 -0300

    Algoritmo pronto

---
+++ index.py
@@ -0,0 +1,54 @@
+
+#   a             a
+#  / \  ==>      / \
+# b   c         c   b
+
+
+def showBinaryTree(head):
+    current = head
+    print(current.value)
+
+    if(current.left != None):
+        showBinaryTree(current.left)
+
+    if(current.right != None):
+        showBinaryTree(current.right)
+
+def invertBinaryTree(head):
+    current = head
+
+    auxNode = current.right
+    current.right = current.left
+    current.left = auxNode
+
+    if(current.left != None):
+        invertBinaryTree(current.left)
+
+    if(current.right != None):
+        invertBinaryTree(current.right)
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+root = BinaryTree(10)
+a0 = BinaryTree(2)
+a1 = BinaryTree(3)
+b0 = BinaryTree(4)
+b1 = BinaryTree(5)
+b2 = BinaryTree(215)
+b3 = BinaryTree(53)
+
+root.left = a0
+root.right = a1
+a0.left = b0
+a0.right = b1
+a1.left = b2
+a1.right = b3
+
+showBinaryTree(root)
+print()
+invertBinaryTree(root)
+showBinaryTree(root)
